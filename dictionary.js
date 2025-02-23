760fb3a22d821ba33af3d8ce97fa78c53dd3c8ce
vulturechoujin <134819645+vulturechoujin@users.noreply.github.com>
2025-02-23 21:08:31 +0700

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,11 @@
+export const testDictionary = ['naruto', 'sasuke', 'sakura', 'sakuta', 'hinata', 'hiyori'];
+
+export const realDictionary = [
+  "naruto",
+  "sasuke",
+  "sakura",
+  "sakuta",
+  "hinata",
+  "hiyori",
+    ];
+export const Suggestion = ["In Naruto, yellow hair","Use Chidori","In naruto, has a strong punch","In bunny girl's senpai, main character","In Naruto, very shy","A female in noragami"];
---
+++ index.js
@@ -0,0 +1,175 @@
+import { testDictionary, realDictionary, Suggestion } from './dictionary.js';
+console.log('test dictionary:', testDictionary);
+
+const dictionary = realDictionary;
+const suggest = Suggestion;
+const pos ={
+    x:Math.floor(Math.random() * dictionary.length),
+};
+const state = {
+  secret: dictionary[pos.x],
+  grid: Array(4)
+    .fill()
+    .map(() => Array(6).fill('')),
+  currentRow: 0,
+  currentCol: 0, idea: suggest[pos.x],
+};
+
+function drawTable(table) {
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 6; j++) {
+      drawBox(grid, i, j);
+    }
+  }
+
+  table.appendChild(grid);
+}
+
+function updateGrid() {
+  for (let i = 0; i < state.grid.length; i++) {
+    for (let j = 0; j < state.grid[i].length; j++) {
+      const box = document.getElementById(`box${i}${j}`);
+      box.textContent = state.grid[i][j];
+    }
+  }
+}
+
+function drawBox(table, row, col, letter = '') {
+  const box = document.createElement('div');
+  box.className = 'box';
+  box.textContent = letter;
+  box.id = `box${row}${col}`;
+
+  table.appendChild(box);
+  return box;
+}
+
+function registerKeyboardEvents() {
+  document.body.onkeydown = (e) => {
+    const key = e.key;
+    if (key === 'Enter') {
+      if (state.currentCol === 6) {
+        const word = getWord();
+        if (checking(word)) {
+          revealWord(word);
+          state.currentRow++;
+          state.currentCol = 0;
+        } else {
+          alert('Not a valid word.');
+        }
+      }
+    }
+    if (key === 'Backspace') {
+      removeLetter();
+    }
+    if (isLetter(key)) {
+      addLetter(key);
+    }
+
+    updateGrid();
+  };
+}
+
+function getWord() {
+  return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
+}
+
+function checking(word) {
+  return dictionary.includes(word);
+}
+
+function getNumOfOccurrencesInWord(word, letter) {
+  let result = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function getPositionOfOccurrence(word, letter, position) {
+  let result = 0;
+  for (let i = 0; i <= position; i++) {
+    if (word[i] === letter) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function revealWord(guess) {
+  const row = state.currentRow;
+  const animation_duration = 500; // ms
+
+  for (let i = 0; i < 6; i++) {
+    const box = document.getElementById(`box${row}${i}`);
+    const letter = box.textContent;
+    const numOfOccurrencesSecret = getNumOfOccurrencesInWord(
+      state.secret,
+      letter
+    );
+    const numOfOccurrencesGuess = getNumOfOccurrencesInWord(guess, letter);
+    const letterPosition = getPositionOfOccurrence(guess, letter, i);
+
+    setTimeout(() => {
+      if (
+        numOfOccurrencesGuess > numOfOccurrencesSecret &&
+        letterPosition > numOfOccurrencesSecret
+      ) {
+        box.classList.add('empty');
+      } else {
+        if (letter === state.secret[i]) {
+          box.classList.add('right');
+        } else if (state.secret.includes(letter)) {
+          box.classList.add('wrong');
+        } else {
+          box.classList.add('empty');
+        }
+      }
+    }, ((i + 1) * animation_duration) / 2);
+
+    box.classList.add('animated');
+    box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
+  }
+
+  const isWinner = state.secret === guess;
+  const isGameOver = state.currentRow === 6;
+
+  setTimeout(() => {
+    if (isWinner) {
+      alert('Congratulations!');
+    } else if (isGameOver) {
+      alert(`Better luck next time! The word was ${state.secret}.`);
+    }
+  }, 4 * animation_duration);
+}
+
+function isLetter(key) {
+  return key.length === 1 && key.match(/[a-z]/i);
+}
+
+function addLetter(letter) {
+  if (state.currentCol === 6) return;
+  state.grid[state.currentRow][state.currentCol] = letter;
+  state.currentCol++;
+}
+
+function removeLetter() {
+  if (state.currentCol === 0) return;
+  state.grid[state.currentRow][state.currentCol - 1] = '';
+  state.currentCol--;
+}
+
+function startup() {
+  const game = document.getElementById('game');
+  document.getElementById("idea").textContent = state.idea;
+  drawTable(game);
+
+  registerKeyboardEvents();
+}
+
+startup();
